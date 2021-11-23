cc29e1c982ffa9b7394d0cff6dc5d8cc42493e26
makerzy <botassammzy@gmail.com>
2021-11-23 01:16:40 -0500

    some fix

--- contracts/CaseMgmnt.sol
+++ contracts/CaseMgmnt.sol
@@ -1,3 +1,7 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-10-17
+*/
+
 pragma solidity 0.8.4;
 
 
@@ -11,9 +15,9 @@ contract CaseManager {
     mapping(address=> bool) public isProsecutor;
     mapping(uint256 => Evidence)  getEvidence; // case_id returns array of evidence struct
     mapping(string => Evidence[]) _getAllEvidence;
-    mapping (uint256 => Case) _getCase;
+    mapping (string => Case) _getCase;
     mapping(address => bool) public claimed;
-    mapping(string => uint256[]) caseEvidenceIds; // get all case evidenceIds with caseId
+
     struct Evidence{
         address personnel;
         string evidenceHash;
@@ -33,6 +37,7 @@ contract CaseManager {
     }
     
     uint256 public evidenceID = 0;
+    
     address public owner;
     event UpdateEvidence(address person, string evidence_hash, string case_id);
     event CaseCompleted( address person, uint256 case_id);
@@ -68,16 +73,16 @@ contract CaseManager {
     event InvestigatorAdded(address admin, address investigator);
     event AdminChanged(address admin, address newAdmin);
     event CaseCompleted(address person, Stage stage);
-    event DeleteCase(address person, uint256 caseId);
+    event DeleteCase(address person, string caseId);
     event Claimed(address person, uint256 value);
     
     function addProsecutor(address _prosecutor) OnlyAdmin external{
-        isProsecutor[_prosecutor] =true;
+        isProsecutor[_prosecutor]= true;
         emit ProsecutorAdded(msg.sender, _prosecutor);
     }
     
     function addFisrtResponder(address responder) OnlyAdmin external{
-        isFirstResponder[responder] =true;
+        isFirstResponder[responder]= true;
         emit FirstResponderAdded(msg.sender, responder);
     }
     
@@ -99,76 +104,63 @@ contract CaseManager {
         
     }
     
- 
-    
-    
-    function getCase(uint256 _caseId) external view returns(Case memory){
+    function getCase(string calldata _caseId) external view returns(Case memory){
         return _getCase[_caseId];
     }
     
+    function getCaseEvidence(string calldata case_id) external view returns(Evidence[] memory){
+        return _getAllEvidence[case_id];
+    }
     
     // caseId as input
     event Initialized(address initializer, string hash, string caseId, uint256 evidenceId);
-    function initializeCase( string calldata evidence_hash, string calldata _caseId) external /*OnlyFirstResponder*/{
-        Evidence memory evidence;
+    function initializeCase( string calldata evidence_hash, string calldata _caseId) external OnlyFirstResponder {
+        require(bytes(evidence_hash).length==46, "bad hash");
         evidenceID +=1;
-        evidence.evidenceHash = evidence_hash;
-        evidence.personnel =msg.sender;
-        evidence.timestamp = block.timestamp;
-        evidence.evidenceId = evidenceID;
-        Case memory _case;
-        _case.caseId = _caseId;
-        _case.stage =Stage.opened;
-        _case.initializer = msg.sender;
-        caseEvidenceIds[_caseId].push(evidenceID);
+        Evidence memory evidence = Evidence({
+            evidenceHash: evidence_hash,
+            timestamp: block.timestamp,
+            evidenceId: evidenceID,
+            personnel: msg.sender
+        });
+        
+        Case memory _case = Case({
+            caseId: _caseId,
+            stage: Stage.opened,
+            initializer: msg.sender
+        });
+        
+        _getCase[_caseId]= _case;
         _getAllEvidence[_caseId].push(evidence);
         emit Initialized(msg.sender,evidence_hash, _caseId, evidenceID );
     }
     
     function updateCase( string calldata _caseId, string calldata evidenceHash) external OnlyResponderOrInvestigator{
+        require(_getCase[_caseId].initializer != address(0), "case does not exist");
         evidenceID +=1;
-         Evidence memory evidence;
-        evidence.evidenceHash = evidenceHash;
-        evidence.personnel =msg.sender;
-        evidence.timestamp = block.timestamp;
-        evidence.evidenceId = evidenceID;
+         Evidence memory evidence = Evidence({
+            evidenceHash: evidenceHash,
+            personnel:msg.sender,
+            timestamp :block.timestamp,
+            evidenceId : evidenceID
+         });
+        
          _getAllEvidence[_caseId].push(evidence);
-        caseEvidenceIds[_caseId].push(evidenceID);
         emit UpdateEvidence(msg.sender, evidenceHash, _caseId);
         
     }
     
     
-    function completeCase(uint256 _caseId) external OnlyProsecutor{
+    function completeCase(string calldata _caseId) external OnlyProsecutor{
         Case storage _case = _getCase[_caseId];
         _case.stage = Stage.closed;
         emit CaseCompleted(msg.sender, Stage.closed);
     }
     
     
-    function deleteCase(uint256 _caseId) external OnlyProsecutor{
+    function deleteCase(string calldata _caseId) external OnlyProsecutor{
         delete _getCase[_caseId];
         emit DeleteCase(msg.sender, _caseId);
-    }
-    
-    
-    receive() external payable {
-        
-    }
-    
-    function getEther() external {
-        require(!claimed[msg.sender], "claimed");
-        (bool success, ) = address(msg.sender).call{value: 0.01 *10**17}("");
-        require(success, "failed");
-        claimed[msg.sender] = true;
-        emit Claimed(msg.sender, 0.01 *10**17);
-    }
-    
-    function getExcess() external OnlyAdmin{
-        
-        (bool success, ) = address(msg.sender).call{value: address(this).balance}("");
-        require(success, "failed");
-         emit Claimed(msg.sender, address(this).balance);
     }
     
 }
